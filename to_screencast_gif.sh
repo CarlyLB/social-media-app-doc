f34515b22d7d28e279530dc89cad09c4d0b6511e
Nikolay Grozev <nikolay@nikolays-mbp.au.ibm.com>
2017-05-16 15:10:09 +1000

    New screencast.

--- to_screencast_gif.sh
+++ to_screencast_gif.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-ffmpeg -i ./img/DemoScreencast.mov -pix_fmt rgb24 -filter:v "setpts=0.75*PTS" -s 1100x600 ./img/DemoScreencast.gif
+ffmpeg -i ./img/DemoScreencast.mov -pix_fmt rgb24 -filter:v "setpts=0.75*PTS" -s 1000x600 ./img/DemoScreencast.gif
 
 # ffmpeg -i ./img/DemoScreencast.mov -pix_fmt rgb24 -filter:v "setpts=0.6*PTS" -b 64k ./img/DemoScreencast.gif
